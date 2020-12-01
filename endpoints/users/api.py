31977b83178d2b455aeb87a96d5e5151a5afa61f
kombajno678 <38471959+kombajno678@users.noreply.github.com>
2020-12-01 19:42:10 +0100

    override put for user

--- endpoints/users/api.py
+++ endpoints/users/api.py
@@ -62,3 +62,38 @@ class UserApi(RootApi):
         except Exception as err:
             print(err)
             return None, 400
+
+    @requires_auth
+    def put(self, **kwargs):
+        user_auth_id = _request_ctx_stack.top.current_user
+        print('USER PUT, user auth id = ' + str(user_auth_id))
+
+        # get by token auth_id
+        existing_user = User.query.filter_by(auth_id=user_auth_id).first()
+        if existing_user:
+            # update values
+            for key, value in request.json.items():
+                if hasattr(existing_user, key):
+                    setattr(existing_user, key, value)
+
+            try:
+                db.session.commit()
+                return User.Schema().jsonify(existing_user)
+            except Exception as err:
+                print(err)
+                return None, 400
+
+        else:
+            print('trying to update non existing user')
+            return None, 404
+
+        new_user = User()
+        new_user.id = None
+        new_user.auth_id = user_auth_id
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+            return User.Schema().jsonify(new_user)
+        except Exception as err:
+            print(err)
+            return None, 400
